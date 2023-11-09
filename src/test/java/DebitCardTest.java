4a36a20684eb10d5ff4ba767bbadb9ebb765c021
Eugene test <eupushkash@ngs.ru>
2023-11-09 18:12:44 +0300

    Readme file

--- src/test/java/DebitCardTest.java
+++ src/test/java/DebitCardTest.java
@@ -1,11 +1,32 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 
 
 
 public class DebitCardTest {
     @BeforeEach
-    void InitialUrl(){
-        open();
+    public void InitialUrl() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    public void shouldFillValidData() {
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Рогов Василий");
+        form.$("[data-test-id=phone] input").setValue("+79990009988");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button__text").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    public void shouldFillValidDataWithHyphen() {gg
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Рогов-Василий");
     }
 }
